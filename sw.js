78d7e9183a95a73bc7ada3dc2bbffec0626977fd
joseluis1212 <35735586+joseluis1212@users.noreply.github.com>
2026-07-04 22:42:31 -0300

    Add files via upload
    Primera versión completa.

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'rcp-guide-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/app.js',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
